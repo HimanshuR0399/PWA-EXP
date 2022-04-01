542a5c8002272b997f25af93b051ece7eceacbf8
surajmoolya <moolyasuraj12@gmail.com>
2022-04-01 13:42:53 +0530

    pwa updates

---
+++ js/app.js
@@ -0,0 +1,3 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then((reg) => {console.log('Service Worker Registered',reg);}).catch((err) => {console.log('Service Worker Failed',err);});
+  }
